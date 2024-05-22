3b501a62cef5d4a03a802f5ddf2aba57a8e9cd86
Kris Johnson <kristofj@d.umn.edu>
2024-05-22 15:11:31 -0500

    initial commit

---
+++ hbd-webapp/vite.config.js
@@ -0,0 +1,22 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+      },
+    },
+  },
+  assetsInclude: ["**/*.csv"], // Add this line to include CSV files as assets
+});
